045ab95155619c00f279c418db180581c86e4b4d
ibragimNogotochki <leha.akimenko2007@gmail.com>
2024-12-11 18:35:44 +0300

    Update Main.java

--- project/src/main/java/com/poit/battle/Main.java
+++ project/src/main/java/com/poit/battle/Main.java
@@ -5,7 +5,9 @@ import com.poit.battle.models.Player;
 public class Main {
 
     static void playGame(Player player1, Player player2) {
-        while(!(player1.isGameOver() || player2.isGameOver()));
+        while(!(player1.isGameOver() || player2.isGameOver())){
+
+        }
     }
 
     public static void main(String[] args) {
