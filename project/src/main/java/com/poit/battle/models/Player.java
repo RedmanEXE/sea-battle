10189aa7567dece9ba678cb180adb5cc40291547
ibragimNogotochki <leha.akimenko2007@gmail.com>
2024-12-11 18:21:08 +0300

    Реализован метод isGameOver в классе Player

---
+++ project/src/main/java/com/poit/battle/models/Player.java
@@ -0,0 +1,20 @@
+package com.poit.battle.models;
+
+public class Player {
+    private Field field;
+
+    public Player(Field field) {
+        this.field = field;
+    }
+
+    public boolean isGameOver(){
+        boolean gameOver = true;
+        for(int i = 0; i < 10 ; i++)
+            for(int j = 0; j < 10; j++)
+                if(field.getBlock(i, j) == Field.Block.SHIP) {
+                    gameOver = false;
+                    break;
+                }
+        return gameOver;
+    }
+}
