fec67518376d358768440c348c7529230bb6df6e
ibragimNogotochki <leha.akimenko2007@gmail.com>
2024-12-11 18:21:21 +0300

    Update Main.java

--- project/src/main/java/com/poit/battle/Main.java
+++ project/src/main/java/com/poit/battle/Main.java
@@ -1,6 +1,13 @@
 package com.poit.battle;
 
+import com.poit.battle.models.Player;
+
 public class Main {
+
+    static void playGame(Player player1, Player player2) {
+        while(!(player1.isGameOver() || player2.isGameOver()));
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
